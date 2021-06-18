9c9e18272ee19e8bb94fa9f4737e56f4ddf2911a
qiqf <723469354@qq.com>
2021-06-18 23:48:40 +0800

    feat: 完善首页Header部分

--- src/components/Header/Operation/index.tsx
+++ src/components/Header/Operation/index.tsx
@@ -1,27 +1,40 @@
-import React, { FC } from "react";
+import React, { FC, CSSProperties } from "react";
 import { Space, Dropdown, Menu, Typography } from "antd";
-import { PlusCircleOutlined } from "@ant-design/icons";
+import { PlusCircleOutlined, BellOutlined } from "@ant-design/icons";
 import "./index.less";
+
+const menuStyle: CSSProperties = {
+  paddingTop: "6px",
+  paddingBottom: "6px",
+};
 
 const Operation: FC = () => {
   const menu = (
-    <Menu>
+    <Menu style={menuStyle}>
       <Menu.Item key="1">新建知识库</Menu.Item>
+      <Menu.Item key="2">新建知识小组</Menu.Item>
     </Menu>
   );
 
   return (
-    <Space className="operation-container">
-      <Dropdown overlay={menu}>
+    <Space
+      className="operation-container"
+      size={24}
+      style={{ verticalAlign: "middle" }}
+    >
+      <Dropdown overlay={menu} arrow placement="topRight">
         <a href="/" className="link" onClick={(e) => e.preventDefault()}>
           <Space>
             <PlusCircleOutlined
               style={{ color: "#25b864", fontSize: "18px" }}
             />
-            <Typography.Text>新建</Typography.Text>
+            <Typography.Text type="secondary">新建</Typography.Text>
           </Space>
         </a>
       </Dropdown>
+      <a href="/" className="link" onClick={(e) => e.preventDefault()}>
+        <BellOutlined style={{ fontSize: "16px" }} />
+      </a>
     </Space>
   );
 };

7aaee5d43cba1fd631f5ac26804edbe96ce27b82
qiqf <723469354@qq.com>
2021-07-02 14:26:45 +0800

    feat: 完善用户info模块

--- src/pages/User/Info/index.tsx
+++ src/pages/User/Info/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Avatar, Card, Typography } from "antd";
+import { Avatar, Card, Typography, Button } from "antd";
+import { Link } from "react-router-dom";
 import "./index.less";
 
 export default function Info() {
@@ -12,6 +13,29 @@ export default function Info() {
           src="https://avatars.githubusercontent.com/u/36329267?v=4"
         />
         <p>qiqf</p>
+      </div>
+      <div className="follows">
+        <Link to="/" className="q-link">
+          <p>
+            <Typography.Title style={{ marginBottom: 0 }} level={3}>
+              2
+            </Typography.Title>
+            <p>关注者</p>
+          </p>
+        </Link>
+        <Link to="/" className="q-link">
+          <p>
+            <Typography.Title style={{ marginBottom: 0 }} level={3}>
+              2
+            </Typography.Title>
+            <p>关注了</p>
+          </p>
+        </Link>
+      </div>
+      <div className="buttons">
+        <Button block className="button">
+          编辑资料
+        </Button>
       </div>
     </Card>
   );
